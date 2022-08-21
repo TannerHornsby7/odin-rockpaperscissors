53c2a85b6cc7e50c3d1b80bdbed8dc33b066dc60
Tanner Hornsby <tannerhornsby7@gmail.com>
2022-08-20 20:19:40 -0500

    Add Scoreboard and track wins, ending at 5

--- rps.js
+++ rps.js
@@ -8,7 +8,32 @@ function getComputerChoice () {
 const display = document.querySelector('div.display');
 const resultimg = document.createElement('img');
 const resultmsg = document.createElement('h2');
+const body = document.querySelector('body')
+const buttons = document.querySelectorAll('button');
+const buttondiv = document.querySelector('.buttons');
 
+
+
+const score = document.querySelector('div.score');
+const tallye = document.createElement('h2');
+const gameovermsg = document.createElement('h2');
+
+gameovermsg.textContent = "Game Over!";
+
+
+let ptally = +score.textContent[0];
+let ctally = +score.textContent[score.textContent.length - 1];
+console.log(ptally);
+console.log(ctally)
+
+function gameOver(pwins, cwins) {
+    if(pwins >= 5 || cwins >= 5) {
+        while (buttondiv.firstChild) {
+            buttondiv.removeChild(buttondiv.firstChild);
+        }
+        buttondiv.appendChild(gameovermsg);  
+    }
+}
 
 function standardize (string) {
     return string[0].toUpperCase() + string.substring(1).toLowerCase();
@@ -35,51 +60,29 @@ function gameLogic (playerSelection, computerSelection) {
     if (win) {
         resultimg.src = "assets/player.jpg";
         resultmsg.textContent = "You Win!!!";
+        ptally++;
     }else if(playerSelection == computerSelection) {
         resultimg.src = "assets/tie.jpg";
         resultmsg.textContent = "It's A Tie";
     } else {
         resultimg.src = "assets/computer.png";
         resultmsg.textContent = "You Lose :(";
+        ctally++;
     }
     while (display.firstChild) {
         display.removeChild(display.firstChild);
     }
+    while (score.firstChild) {
+        score.removeChild(score.firstChild);
+    }
+
+    gameOver(ptally, ctally);
+
+    tallye.textContent = ptally + " to " + ctally;
+    score.appendChild(tallye);
     display.appendChild(resultimg);
     display.appendChild(resultmsg);
 }
-
-function game () {
-    let game_count = 0;
-    let p_wins = 0;
-    let c_wins = 0;
-    let winner = "Everybody :)"
-    while(game_count < 5) {
-        let computerSelection = getComputerChoice();
-        let playerSelection = prompt("Enter Rock, Paper, or Scissors!");
-        let round = gameLogic(playerSelection, computerSelection);
-        console.log(round);
-        alert(round);
-
-        if (round == "You Lose :(") {
-            c_wins++;
-        }
-        if (round == "You Win!!!") {
-            p_wins++;
-        }
-        game_count++;
-    }
-
-    if(p_wins > c_wins) {
-        winner = "You!!!";
-    }
-    if(p_wins < c_wins) {
-        winner = "Computer >:)";
-    }
-    return alert("The winner is: " + winner);
-}
-
-const buttons = document.querySelectorAll('button');
 
 buttons.forEach(button => {
     button.addEventListener('click', e => console.log(gameLogic(e.target.classList.value, getComputerChoice())));
